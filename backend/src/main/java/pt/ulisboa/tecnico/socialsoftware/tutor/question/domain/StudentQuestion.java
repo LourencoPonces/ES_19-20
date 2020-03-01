f7961ae3007b2f2093ff2cc1299be40bae8d8cae
Beu-Wolf <daniel.gseara@gmail.com>
2020-03-01 10:31:19 +0000

    StudentQuestion specialization of Question
    Added StudentQuestion domain entity and relations to User and Question

---
+++ backend/src/main/java/pt/ulisboa/tecnico/socialsoftware/tutor/question/domain/StudentQuestion.java
@@ -0,0 +1,47 @@
+package pt.ulisboa.tecnico.socialsoftware.tutor.question.domain;
+
+
+import javax.persistence.*;
+
+import pt.ulisboa.tecnico.socialsoftware.tutor.question.dto.QuestionDto;
+import pt.ulisboa.tecnico.socialsoftware.tutor.user.User;
+import pt.ulisboa.tecnico.socialsoftware.tutor.course.Course;
+
+
+@Entity
+public class StudentQuestion extends Question {
+
+    public enum SubmittedStatus {
+        WAITING_FOR_APPROVAL, REJECTED, APPROVED
+    }
+
+    @Column
+    private String justification = "";
+
+    @Enumerated(EnumType.STRING)
+    private SubmittedStatus submittedStatus = SubmittedStatus.WAITING_FOR_APPROVAL;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+
+
+    public StudentQuestion() {
+
+    }
+
+    public StudentQuestion(Course course, QuestionDto questionDto, User student) {
+        super(course, questionDto);
+        user = student;
+    }
+
+    public String getJustification() { return justification; }
+    public void setJustification(String justification) { this.justification = justification; }
+
+    public User getUser() { return user; }
+    public void setUser(User user) { this.user = user; }
+
+    public SubmittedStatus getSubmittedStatus() { return submittedStatus; }
+    public void setSubmittedStatus(SubmittedStatus status) { this.submittedStatus = status; }
+}
