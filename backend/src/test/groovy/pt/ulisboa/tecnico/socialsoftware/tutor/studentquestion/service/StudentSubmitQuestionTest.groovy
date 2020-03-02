ef680623201312f3aa127909f5adc92b7077a304
Beu-Wolf <daniel.gseara@gmail.com>
2020-03-02 23:24:09 +0000

    Refactor test cases and add Bean

--- backend/src/test/groovy/pt/ulisboa/tecnico/socialsoftware/tutor/studentquestion/service/StudentSubmitQuestionTest.groovy
+++ backend/src/test/groovy/pt/ulisboa/tecnico/socialsoftware/tutor/studentquestion/service/StudentSubmitQuestionTest.groovy
@@ -1,14 +1,19 @@
 package pt.ulisboa.tecnico.socialsoftware.tutor.studentquestion.service
 
+import org.springframework.beans.factory.annotation.Autowire
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.TestConfiguration
+import org.springframework.context.annotation.Bean
 import pt.ulisboa.tecnico.socialsoftware.tutor.question.StudentSubmitQuestionService
 import spock.lang.Specification
 
 class StudentSubmitQuestionTest extends Specification {
 
-    def studentSubmitQuestionService
+    @Autowired
+    StudentSubmitQuestionService studentSubmitQuestionService
 
     def setup() {
-        studentSubmitQuestionService = new StudentSubmitQuestionService()
+
     }
 
     def "create new question with 1 option and 1 Topic and submit for approval"() {
@@ -21,19 +26,33 @@ class StudentSubmitQuestionTest extends Specification {
         expect: false
     }
 
-    def "create 2 new questions with 1 Topic each"() {
+    def "create 2 new questions with 1 Topic each and submit"() {
         // question created and submitted
         expect: false
     }
 
-    def "create question without options"() {
+    def "create question without options and submit"() {
         // exception thrown
         expect: false
     }
 
-    def "create question without Topics"() {
+    def "create question and without correct option and submit"() {
         // exception thrown
         expect: false
+    }
+
+    def "create question without Topics and submit"() {
+        // exception thrown
+        expect: false
+    }
+
+    @TestConfiguration
+    static class StudentQuestionServiceImplTestContextConfiguration {
+
+        @Bean
+        StudentSubmitQuestionService studentSubmitQuestionService() {
+            return  new StudentSubmitQuestionService();
+        }
     }
 
 }
