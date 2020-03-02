6bc3411ea981fe40900cc3480cedb82090dd9d06
Beu-Wolf <daniel.gseara@gmail.com>
2020-03-02 23:26:30 +0000

    PpA feature3 Service

---
+++ backend/src/main/java/pt/ulisboa/tecnico/socialsoftware/tutor/question/StudentCheckQuestionStatusService.java
@@ -0,0 +1,11 @@
+package pt.ulisboa.tecnico.socialsoftware.tutor.question;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class StudentCheckQuestionStatusService {
+    public void StucdentCheckQuestionStatus() {
+
+    }
+
+}
