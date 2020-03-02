3224493ba2a38786b5e7d351d7c8d418b3ffa45c
Beu-Wolf <daniel.gseara@gmail.com>
2020-03-02 23:25:06 +0000

    added Service to work with Spring beans

--- backend/src/main/java/pt/ulisboa/tecnico/socialsoftware/tutor/question/StudentSubmitQuestionService.java
+++ backend/src/main/java/pt/ulisboa/tecnico/socialsoftware/tutor/question/StudentSubmitQuestionService.java
@@ -1,5 +1,8 @@
 package pt.ulisboa.tecnico.socialsoftware.tutor.question;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class StudentSubmitQuestionService {
     public void StudentSubmitQuestion() {
 
