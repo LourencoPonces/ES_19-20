ecc18339cde3b5b0db4477dd2b6e3a45d2ff7ef8
Beu-Wolf <daniel.gseara@gmail.com>
2020-03-04 18:13:33 +0000

    Join with User table

--- backend/src/main/java/pt/ulisboa/tecnico/socialsoftware/tutor/question/domain/StudentQuestion.java
+++ backend/src/main/java/pt/ulisboa/tecnico/socialsoftware/tutor/question/domain/StudentQuestion.java
@@ -24,7 +24,7 @@ public class StudentQuestion extends Question {
     private SubmittedStatus submittedStatus = SubmittedStatus.WAITING_FOR_APPROVAL;
 
     @ManyToOne
-    @JoinColumn(name = "user_id")
+    @JoinColumn(name = "user_id", nullable = false)
     private User user;
 
 
