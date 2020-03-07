06de8a20b2376298b2252295eed83e3dbc7247dd
Marcelo Santos <marcelocmsantos@gmail.com>
2020-03-07 23:03:54 +0000

    Create Tournament model

---
+++ backend/src/main/java/pt/ulisboa/tecnico/socialsoftware/tutor/tournament/domain/Tournament.java
@@ -0,0 +1,121 @@
+package pt.ulisboa.tecnico.socialsoftware.tutor.tournament.domain;
+
+import pt.ulisboa.tecnico.socialsoftware.tutor.course.CourseExecution;
+import pt.ulisboa.tecnico.socialsoftware.tutor.question.domain.Topic;
+import pt.ulisboa.tecnico.socialsoftware.tutor.tournament.dto.TournamentDto;
+import pt.ulisboa.tecnico.socialsoftware.tutor.user.User;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "tournaments")
+public class Tournament {
+    public enum Status {AVAILABLE, RUNNING, FINISHED, CANCELLED}
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(unique=true, nullable = false)
+    private Integer key;
+
+    @Column(name = "creation_date")
+    private LocalDateTime creationDate;
+
+    @Column(name = "available_date")
+    private LocalDateTime availableDate;
+
+    @Column(name = "conclusion_date")
+    private LocalDateTime conclusionDate;
+
+    @Column(nullable = false)
+    private String title = "Title"; // TODO: why "Title"?
+
+    @Enumerated(EnumType.STRING)
+    private Status status;
+
+    @ManyToMany(mappedBy = "tournaments")
+    private Set<Topic> topics = new HashSet<>();
+
+    @ManyToOne
+    @JoinColumn(name = "course_execution_id")
+    private CourseExecution courseExecution;
+
+    @ManyToOne(fetch=FetchType.LAZY)
+    @JoinColumn(name = "creator_id")
+    private User creator;
+
+    @ManyToMany(mappedBy = "tournaments")
+    private Set<User> participants;
+
+    public Tournament() {}
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getKey() {
+        return key;
+    }
+
+    public void setKey(Integer key) {
+        this.key = key;
+    }
+
+    public LocalDateTime getCreationDate() {
+        return creationDate;
+    }
+
+    public void setCreationDate(LocalDateTime creationDate) {
+        this.creationDate = creationDate;
+    }
+
+    public LocalDateTime getAvailableDate() {
+        return availableDate;
+    }
+
+    public void setAvailableDate(LocalDateTime availableDate) {
+        this.availableDate = availableDate;
+    }
+
+    public LocalDateTime getConclusionDate() {
+        return conclusionDate;
+    }
+
+    public void setConclusionDate(LocalDateTime conclusionDate) {
+        this.conclusionDate = conclusionDate;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        checkTitle(title);
+        this.title = title;
+    }
+
+    @Override
+    public String toString() {
+        // TODO
+        return "Tournament{" +
+                "id=" + id +
+                ", creationDate=" + creationDate +
+                ", availableDate=" + availableDate +
+                ", conclusionDate=" + conclusionDate +
+                ", title='" + title + '\'' +
+                ", status=" + status +
+                '}';
+    }
+
+    private void checkTitle(String title) {
+        // TODO
+    }
+}
