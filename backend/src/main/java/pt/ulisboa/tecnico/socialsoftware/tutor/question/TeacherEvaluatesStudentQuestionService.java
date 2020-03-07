9d2824345940955003206355fbfb3d6477222044
afonsocrg <afonso.crg@gmail.com>
2020-03-07 11:13:02 +0000

    added service interface

--- backend/src/main/java/pt/ulisboa/tecnico/socialsoftware/tutor/question/TeacherEvaluatesStudentQuestionService.java
+++ backend/src/main/java/pt/ulisboa/tecnico/socialsoftware/tutor/question/TeacherEvaluatesStudentQuestionService.java
@@ -1,10 +1,27 @@
 package pt.ulisboa.tecnico.socialsoftware.tutor.question;
 
 import org.springframework.stereotype.Service;
+import pt.ulisboa.tecnico.socialsoftware.tutor.question.domain.StudentQuestion;
 
 @Service
 public class TeacherEvaluatesStudentQuestionService {
     public void TeacherEvaluatesStudentQuestionService() {
+
+    }
+
+    public void acceptStudentQuestion(StudentQuestion s) {
+
+    }
+
+    public void acceptStudentQuestion(StudentQuestion s, String justification) {
+
+    }
+
+    public void rejectStudentQuestion(StudentQuestion s) {
+
+    }
+
+    public void rejectStudentQuestion(StudentQuestion s, String justification) {
 
     }
 }
