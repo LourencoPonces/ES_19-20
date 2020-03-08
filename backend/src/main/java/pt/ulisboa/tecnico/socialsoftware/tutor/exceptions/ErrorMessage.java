e2c9ef047d3af627406401b0a4581f5461bc47a5
afonsocrg <afonso.crg@gmail.com>
2020-03-08 18:49:36 +0000

    add studentQuestionNotFound error

--- backend/src/main/java/pt/ulisboa/tecnico/socialsoftware/tutor/exceptions/ErrorMessage.java
+++ backend/src/main/java/pt/ulisboa/tecnico/socialsoftware/tutor/exceptions/ErrorMessage.java
@@ -7,6 +7,7 @@ public enum ErrorMessage {
     QUESTION_ANSWER_NOT_FOUND("Question answer not found with id %d"),
     OPTION_NOT_FOUND("Option not found with id %d"),
     QUESTION_NOT_FOUND("Question not found with id %d"),
+    STUDENT_QUESTION_NOT_FOUND("Student Question not found with id %d"),
     USER_NOT_FOUND("User not found with id %d"),
     TOPIC_NOT_FOUND("Topic not found with id %d"),
     NO_TOPICS("The question has no Topics"),
