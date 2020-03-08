e0a56631b0979352ce4e27370ab5f0a251346e31
lourenco <lourenco.poncesduarte@gmail.com>
2020-03-08 18:45:22 +0000

    Added createTournament method in TournamentService

--- backend/src/main/java/pt/ulisboa/tecnico/socialsoftware/tutor/tournament/TournamentService.java
+++ backend/src/main/java/pt/ulisboa/tecnico/socialsoftware/tutor/tournament/TournamentService.java
@@ -1,8 +1,13 @@
 package pt.ulisboa.tecnico.socialsoftware.tutor.tournament;
 
 import org.springframework.stereotype.Service;
+import pt.ulisboa.tecnico.socialsoftware.tutor.tournament.domain.Tournament;
+import pt.ulisboa.tecnico.socialsoftware.tutor.tournament.dto.TournamentDto;
 
 @Service
 public class TournamentService {
 
+    public TournamentDto createTournament(){
+        return null;
+    }
 }
