f90830538e5c18567cf945067758dd431b075043
afonsocrg <afonso.crg@gmail.com>
2020-03-08 19:06:44 +0000

    defined rejection tests

--- backend/src/test/groovy/pt/ulisboa/tecnico/socialsoftware/tutor/studentquestion/service/TeacherRejectsStudentQuestionTest.groovy
+++ backend/src/test/groovy/pt/ulisboa/tecnico/socialsoftware/tutor/studentquestion/service/TeacherRejectsStudentQuestionTest.groovy
@@ -1,15 +1,114 @@
 package pt.ulisboa.tecnico.socialsoftware.tutor.studentquestion.service
 
-import pt.ulisboa.tecnico.socialsoftware.tutor.question.StudentSubmitQuestionService
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest
+import org.springframework.boot.test.context.TestConfiguration
+import org.springframework.context.annotation.Bean
+import pt.ulisboa.tecnico.socialsoftware.tutor.course.Course
+import pt.ulisboa.tecnico.socialsoftware.tutor.course.CourseExecution
+import pt.ulisboa.tecnico.socialsoftware.tutor.course.CourseExecutionRepository
+import pt.ulisboa.tecnico.socialsoftware.tutor.course.CourseRepository
+import pt.ulisboa.tecnico.socialsoftware.tutor.question.TeacherEvaluatesStudentQuestionService
+import pt.ulisboa.tecnico.socialsoftware.tutor.question.domain.Option
+import pt.ulisboa.tecnico.socialsoftware.tutor.question.domain.StudentQuestion
+import pt.ulisboa.tecnico.socialsoftware.tutor.question.domain.Topic
+import pt.ulisboa.tecnico.socialsoftware.tutor.question.repository.StudentQuestionRepository
+import pt.ulisboa.tecnico.socialsoftware.tutor.user.User
+import pt.ulisboa.tecnico.socialsoftware.tutor.user.UserRepository
 import spock.lang.Specification
 
+@DataJpaTest
 class TeacherRejectsStudentQuestionTest extends Specification {
 
-    // def studentSubmitQuestionService
+
+    public static final String COURSE_NAME = "Software Architecture"
+
+    public static final String ACRONYM = "AS1"
+    public static final String ACADEMIC_TERM = "1 SEM"
+
+    public static final String USER_NAME = "ist199999"
+
+    public static final Integer STUDENT_QUESTION_KEY = 1
+    public static final Integer FAKE_STUDENT_QUESTION_KEY = 2
+
+    public static final String JUSTIFICATION = "very good question"
+
+    @Autowired
+    TeacherEvaluatesStudentQuestionService teacherEvaluatesStudentQuestionService
+
+
+    @Autowired
+    CourseRepository courseRepository
+
+    @Autowired
+    CourseExecutionRepository courseExecutionRepository
+
+    @Autowired
+    StudentQuestionRepository studentQuestionRepository
+
+    @Autowired
+    UserRepository userRepository
+
 
     def setup() {
-        // studentSubmitQuestionService = new StudentSubmitQuestionService()
+        def course = new Course(COURSE_NAME, Course.Type.TECNICO)
+        courseRepository.save(course)
+
+        // course execution
+        def courseExecution = new CourseExecution(course, ACRONYM, ACADEMIC_TERM, Course.Type.TECNICO)
+        courseExecutionRepository.save(courseExecution)
+
+        // user
+        def user = new User()
+        user.setKey(1)
+        user.setUsername(USER_NAME)
+        user.getCourseExecutions().add(courseExecution)
+        userRepository.save(user)
+
+        // options
+        def option = new Option()
+
+        // topic
+        def topic = new Topic()
+
+        // studentQuestion
+        def studentQuestion = new StudentQuestion()
+        studentQuestion.addTopic(topic)
+        studentQuestion.addOption(option)
+        studentQuestion.setKey(STUDENT_QUESTION_KEY)
+        studentQuestion.setStudentQuestionKey(STUDENT_QUESTION_KEY)
+        studentQuestion.setUser(user)
+        studentQuestion.setCourse(course)
+
+        // save studentQuestion
+        studentQuestionRepository.save(studentQuestion)
+    }
+
+    def "reject student question with valid justification"() {
+        expect: false
+    }
+
+    // impossible to reject question with no justification parameter
+    def "reject student question with invalid justification"() {
+        expect: false
+    }
+
+    def "reject already evaluated student question"() {
+        expect: false
+    }
+
+    def "reject non existing student question"() {
+        expect: false
     }
 
 
+
+    @TestConfiguration
+    static class TeacherEvaluatesImplTestContextConfiguration {
+
+        @Bean
+        TeacherEvaluatesStudentQuestionService teacherEvaluatesStudentQuestionService() {
+            return  new TeacherEvaluatesStudentQuestionService();
+        }
+    }
 }
