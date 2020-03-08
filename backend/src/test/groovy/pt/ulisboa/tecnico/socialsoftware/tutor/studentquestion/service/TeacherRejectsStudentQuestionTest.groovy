8d4e2203b3b16ee1461997f4a4e722b30b9fe586
afonsocrg <afonso.crg@gmail.com>
2020-03-08 19:26:31 +0000

    implemented rejection tests

--- backend/src/test/groovy/pt/ulisboa/tecnico/socialsoftware/tutor/studentquestion/service/TeacherRejectsStudentQuestionTest.groovy
+++ backend/src/test/groovy/pt/ulisboa/tecnico/socialsoftware/tutor/studentquestion/service/TeacherRejectsStudentQuestionTest.groovy
@@ -8,6 +8,7 @@ import pt.ulisboa.tecnico.socialsoftware.tutor.course.Course
 import pt.ulisboa.tecnico.socialsoftware.tutor.course.CourseExecution
 import pt.ulisboa.tecnico.socialsoftware.tutor.course.CourseExecutionRepository
 import pt.ulisboa.tecnico.socialsoftware.tutor.course.CourseRepository
+import pt.ulisboa.tecnico.socialsoftware.tutor.exceptions.TutorException
 import pt.ulisboa.tecnico.socialsoftware.tutor.question.TeacherEvaluatesStudentQuestionService
 import pt.ulisboa.tecnico.socialsoftware.tutor.question.domain.Option
 import pt.ulisboa.tecnico.socialsoftware.tutor.question.domain.StudentQuestion
@@ -16,6 +17,11 @@ import pt.ulisboa.tecnico.socialsoftware.tutor.question.repository.StudentQuesti
 import pt.ulisboa.tecnico.socialsoftware.tutor.user.User
 import pt.ulisboa.tecnico.socialsoftware.tutor.user.UserRepository
 import spock.lang.Specification
+
+
+import static pt.ulisboa.tecnico.socialsoftware.tutor.exceptions.ErrorMessage.INVALID_JUSTIFICATION
+import static pt.ulisboa.tecnico.socialsoftware.tutor.exceptions.ErrorMessage.CANNOT_REJECT_ACCEPTED_SUGGESTION
+import static pt.ulisboa.tecnico.socialsoftware.tutor.exceptions.ErrorMessage.STUDENT_QUESTION_NOT_FOUND
 
 @DataJpaTest
 class TeacherRejectsStudentQuestionTest extends Specification {
@@ -31,7 +37,7 @@ class TeacherRejectsStudentQuestionTest extends Specification {
     public static final Integer STUDENT_QUESTION_KEY = 1
     public static final Integer FAKE_STUDENT_QUESTION_KEY = 2
 
-    public static final String JUSTIFICATION = "very good question"
+    public static final String VALID_JUSTIFICATION = "irrelevant question"
 
     @Autowired
     TeacherEvaluatesStudentQuestionService teacherEvaluatesStudentQuestionService
@@ -85,23 +91,81 @@ class TeacherRejectsStudentQuestionTest extends Specification {
     }
 
     def "reject student question with valid justification"() {
-        expect: false
+        when:
+        teacherEvaluatesStudentQuestionService.rejectStudentQuestion(STUDENT_QUESTION_KEY, VALID_JUSTIFICATION)
+
+        then:
+        studentQuestionRepository.count() == 1L
+        def result = studentQuestionRepository.findAll().get(0)
+        result.getSubmittedStatus() == StudentQuestion.SubmittedStatus.REJECTED
+        result.getJustification() == VALID_JUSTIFICATION
     }
 
     // impossible to reject question with no justification parameter
     def "reject student question with invalid justification"() {
-        expect: false
+        when:
+        teacherEvaluatesStudentQuestionService.rejectStudentQuestion(STUDENT_QUESTION_KEY, justification)
+
+        then:
+        def error = thrown(TutorException)
+        error.errorMessage == INVALID_JUSTIFICATION
+
+
+        // invalid justifications:
+        // * empty strings
+        where:
+        justification || result
+        ""            || INVALID_JUSTIFICATION
+        "   "         || INVALID_JUSTIFICATION
     }
 
-    def "reject already evaluated student question"() {
-        expect: false
+
+    def "reject already accepted student question"() {
+        given: 'pending student question'
+        studentQuestionRepository.count() == 1L
+        def question = studentQuestionRepository.findAll().get(0)
+        evaluateQuestion(true, question)
+
+
+        when:
+        teacherEvaluatesStudentQuestionService.rejectStudentQuestion(STUDENT_QUESTION_KEY, VALID_JUSTIFICATION)
+
+        then:
+        def error = thrown(TutorException)
+        error.errorMessage == CANNOT_REJECT_ACCEPTED_SUGGESTION
+    }
+
+    def "reject already rejected student question"() {
+        given: 'pending student question'
+        studentQuestionRepository.count() == 1L
+        def question = studentQuestionRepository.findAll().get(0)
+        evaluateQuestion(false, question)
+
+
+        when:
+        teacherEvaluatesStudentQuestionService.rejectStudentQuestion(STUDENT_QUESTION_KEY, VALID_JUSTIFICATION)
+
+        then:
+        studentQuestionRepository.findAll().get(0).getSubmittedStatus() == StudentQuestion.SubmittedStatus.REJECTED
     }
 
     def "reject non existing student question"() {
-        expect: false
+        when:
+        teacherEvaluatesStudentQuestionService.rejectStudentQuestion(FAKE_STUDENT_QUESTION_KEY, VALID_JUSTIFICATION)
+
+        then:
+        def error = thrown(TutorException)
+        error.errorMessage == STUDENT_QUESTION_NOT_FOUND
     }
 
 
+    def evaluateQuestion(isAccepted, question) {
+        if(isAccepted) {
+            question.setSubmittedStatus(StudentQuestion.SubmittedStatus.APPROVED)
+        } else {
+            question.setSubmittedStatus(StudentQuestion.SubmittedStatus.REJECTED)
+        }
+    }
 
     @TestConfiguration
     static class TeacherEvaluatesImplTestContextConfiguration {
