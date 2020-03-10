784b6c5102f78c3555712c39b54aea30283e7efe
afonsocrg <afonso.crg@gmail.com>
2020-03-10 11:28:56 +0000

    define service interface

--- backend/src/main/java/pt/ulisboa/tecnico/socialsoftware/tutor/question/CheckStudentQuestionStatusService.java
+++ backend/src/main/java/pt/ulisboa/tecnico/socialsoftware/tutor/question/CheckStudentQuestionStatusService.java
@@ -2,6 +2,9 @@ package pt.ulisboa.tecnico.socialsoftware.tutor.question;
 
 import org.springframework.stereotype.Service;
 import pt.ulisboa.tecnico.socialsoftware.tutor.question.domain.StudentQuestion;
+import pt.ulisboa.tecnico.socialsoftware.tutor.question.dto.StudentQuestionDTO;
+
+import java.util.Set;
 
 @Service
 public class CheckStudentQuestionStatusService {
@@ -9,5 +12,13 @@ public class CheckStudentQuestionStatusService {
     public StudentQuestion.SubmittedStatus getStudentQuestionStatus(Integer studentQuestionId) {
         return StudentQuestion.SubmittedStatus.APPROVED;
     }
+
+    public Set<StudentQuestionDTO> getAllStudentQuestion(Integer studentId) {}
+
+    public Set<StudentQuestionDTO> getAllApprovedStudentQuestions(Integer studentId) {}
+
+    public Set<StudentQuestionDTO> getAllRejectedStudentQuestions(Integer studentId) {}
+
+    public Set<StudentQuestionDTO> getAllPendingStudentQuestions(Integer studentId) {}
 
 }
