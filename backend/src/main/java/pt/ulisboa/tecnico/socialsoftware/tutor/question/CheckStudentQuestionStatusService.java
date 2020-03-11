8d23cd0c14eee6bb6660a9717d47847b1df81340
afonsocrg <afonso.crg@gmail.com>
2020-03-11 11:10:36 +0000

    return type from set to list; crush filter function into single one

--- backend/src/main/java/pt/ulisboa/tecnico/socialsoftware/tutor/question/CheckStudentQuestionStatusService.java
+++ backend/src/main/java/pt/ulisboa/tecnico/socialsoftware/tutor/question/CheckStudentQuestionStatusService.java
@@ -4,8 +4,8 @@ import org.springframework.stereotype.Service;
 import pt.ulisboa.tecnico.socialsoftware.tutor.question.domain.StudentQuestion;
 import pt.ulisboa.tecnico.socialsoftware.tutor.question.dto.StudentQuestionDTO;
 
-import java.util.Set;
-import java.util.TreeSet;
+import java.util.List;
+import java.util.ArrayList;
 
 @Service
 public class CheckStudentQuestionStatusService {
@@ -14,20 +14,12 @@ public class CheckStudentQuestionStatusService {
         return StudentQuestion.SubmittedStatus.APPROVED;
     }
 
-    public Set<StudentQuestionDTO> getAllStudentQuestion(Integer studentId) {
-        return new TreeSet<StudentQuestionDTO>();
+    public List<StudentQuestionDTO> getAllStudentQuestion(Integer studentId) {
+        return new ArrayList<StudentQuestionDTO>();
     }
 
-    public Set<StudentQuestionDTO> getAllApprovedStudentQuestions(Integer studentId) {
-        return new TreeSet<StudentQuestionDTO>();
-    }
-
-    public Set<StudentQuestionDTO> getAllRejectedStudentQuestions(Integer studentId) {
-        return new TreeSet<StudentQuestionDTO>();
-    }
-
-    public Set<StudentQuestionDTO> getAllPendingStudentQuestions(Integer studentId) {
-        return new TreeSet<StudentQuestionDTO>();
+    public List<StudentQuestionDTO> getAllQuestionsWithStatus(Integer studentId, StudentQuestion.SubmittedStatus status) {
+        return new ArrayList<StudentQuestionDTO>();
     }
 
 }
