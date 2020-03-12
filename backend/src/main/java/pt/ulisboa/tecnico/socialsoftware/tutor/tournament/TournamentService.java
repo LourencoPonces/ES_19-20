d2d37c07812bf5c63ac46873faeeaa797f4c64cf
Marcelo Santos <marcelocmsantos@gmail.com>
2020-03-12 23:01:14 +0000

    Simplify foreach with for

--- backend/src/main/java/pt/ulisboa/tecnico/socialsoftware/tutor/tournament/TournamentService.java
+++ backend/src/main/java/pt/ulisboa/tecnico/socialsoftware/tutor/tournament/TournamentService.java
@@ -11,6 +11,7 @@ import pt.ulisboa.tecnico.socialsoftware.tutor.course.CourseExecutionRepository;
 import pt.ulisboa.tecnico.socialsoftware.tutor.course.CourseRepository;
 import pt.ulisboa.tecnico.socialsoftware.tutor.exceptions.TutorException;
 import pt.ulisboa.tecnico.socialsoftware.tutor.question.domain.Topic;
+import pt.ulisboa.tecnico.socialsoftware.tutor.question.dto.TopicDto;
 import pt.ulisboa.tecnico.socialsoftware.tutor.question.repository.TopicRepository;
 import pt.ulisboa.tecnico.socialsoftware.tutor.tournament.domain.Tournament;
 import pt.ulisboa.tecnico.socialsoftware.tutor.tournament.dto.TournamentDto;
@@ -102,15 +103,13 @@ public class TournamentService {
     }
 
     private void checkTopics(TournamentDto tournamentDto, CourseExecution courseExecution, Tournament tournament) {
-        tournamentDto.getTopics().stream().forEach(t -> {
-            Topic tmp = topicRepository.findTopicByName(
-                    courseExecution.getCourse().getId(),
-                    t.getName());
-            if (tmp == null) {
+        for(TopicDto t: tournamentDto.getTopics()) {
+            Topic topic = topicRepository.findTopicByName(courseExecution.getCourse().getId(), t.getName());
+            if (topic == null) {
                 throw new TutorException(TOPIC_NOT_FOUND, t.getId());
             } else {
-                tournament.addTopic(tmp);
+                tournament.addTopic(topic);
             }
-        });
+        }
     }
 }
