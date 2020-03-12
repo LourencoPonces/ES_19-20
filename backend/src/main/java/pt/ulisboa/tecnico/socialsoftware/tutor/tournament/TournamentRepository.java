848b4ec9b398ae7465bb7cf3bfee59f067121af4
lourenco <lourenco.poncesduarte@gmail.com>
2020-03-12 18:46:08 +0000

    TdP-F2: Changes in TournamentRepository

--- backend/src/main/java/pt/ulisboa/tecnico/socialsoftware/tutor/tournament/TournamentRepository.java
+++ backend/src/main/java/pt/ulisboa/tecnico/socialsoftware/tutor/tournament/TournamentRepository.java
@@ -11,21 +11,21 @@ import java.util.List;
 @Repository
 @Transactional
 public interface TournamentRepository extends JpaRepository<Tournament, Integer> {
-    @Query(value = "select * from tounaments t where t.course_execution_id = :courseId", nativeQuery = true)
-    List<Tournament> findTournament(int courseId);
+    @Query(value = "select * from tounaments t where t.course_execution_id = :executionId", nativeQuery = true)
+    List<Tournament> findTournament(int executionId);
 
-    @Query(value = "select * from tounaments t where t.course_execution_id = :courseId and t.status ='CREATED'", nativeQuery = true)
-    List<Tournament> findCratedTournament(int courseId);
+    @Query(value = "select * from tounaments t where t.course_execution_id = :executionId and t.status ='CREATED'", nativeQuery = true)
+    List<Tournament> findCratedTournament(int executionId);
 
-    @Query(value = "select * from tounaments t where t.course_execution_id = :courseId and t.status ='AVAILABLE'", nativeQuery = true)
-    List<Tournament> findAvailableTournament(int courseId);
+    @Query(value = "select * from tounaments t where t.course_execution_id = :executionId and t.status ='AVAILABLE'", nativeQuery = true)
+    List<Tournament> findAvailableTournament(int executionId);
 
-    @Query(value = "select * from tounaments t where t.course_execution_id = :courseId and t.status ='RUNNING'", nativeQuery = true)
-    List<Tournament> findRunningTournament(int courseId);
+    @Query(value = "select * from tounaments t where t.course_execution_id = :executionId and t.status ='RUNNING'", nativeQuery = true)
+    List<Tournament> findRunningTournament(int executionId);
 
-    @Query(value = "select * from tounaments t where t.course_execution_id = :courseId and t.status ='FINISHED'", nativeQuery = true)
-    List<Tournament> findFinishedTournament(int courseId);
+    @Query(value = "select * from tounaments t where t.course_execution_id = :executionId and t.status ='FINISHED'", nativeQuery = true)
+    List<Tournament> findFinishedTournament(int executionId);
 
-    @Query(value = "select * from tounaments t where t.course_execution_id = :courseId and t.status ='CANCELED'", nativeQuery = true)
-    List<Tournament> findCanceledTournament(int courseId);
+    @Query(value = "select * from tounaments t where t.course_execution_id = :executionId and t.status ='CANCELED'", nativeQuery = true)
+    List<Tournament> findCanceledTournament(int executionId);
 }
