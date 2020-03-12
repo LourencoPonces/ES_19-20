9e2bc09bec52bba89f73a407e2792cb7642466fa
lourenco <lourenco.poncesduarte@gmail.com>
2020-03-12 18:22:57 +0000

    TdP-F2: Repository done

--- backend/src/main/java/pt/ulisboa/tecnico/socialsoftware/tutor/tournament/TournamentRepository.java
+++ backend/src/main/java/pt/ulisboa/tecnico/socialsoftware/tutor/tournament/TournamentRepository.java
@@ -1,11 +1,31 @@
 package pt.ulisboa.tecnico.socialsoftware.tutor.tournament;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 import pt.ulisboa.tecnico.socialsoftware.tutor.tournament.domain.Tournament;
 
+import java.util.List;
+
 @Repository
 @Transactional
 public interface TournamentRepository extends JpaRepository<Tournament, Integer> {
+    @Query(value = "select * from tounaments t where t.course_execution_id = :courseId", nativeQuery = true)
+    List<Tournament> findTournament(int courseId);
+
+    @Query(value = "select * from tounaments t where t.course_execution_id = :courseId and t.status ='CREATED'", nativeQuery = true)
+    List<Tournament> findCratedTournament(int courseId);
+
+    @Query(value = "select * from tounaments t where t.course_execution_id = :courseId and t.status ='AVAILABLE'", nativeQuery = true)
+    List<Tournament> findAvailableTournament(int courseId);
+
+    @Query(value = "select * from tounaments t where t.course_execution_id = :courseId and t.status ='RUNNING'", nativeQuery = true)
+    List<Tournament> findRunningTournament(int courseId);
+
+    @Query(value = "select * from tounaments t where t.course_execution_id = :courseId and t.status ='FINISHED'", nativeQuery = true)
+    List<Tournament> findFinishedTournament(int courseId);
+
+    @Query(value = "select * from tounaments t where t.course_execution_id = :courseId and t.status ='CANCELED'", nativeQuery = true)
+    List<Tournament> findCanceledTournament(int courseId);
 }
