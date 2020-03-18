eef7bc9a875672d8c7bae144242af4f62ece210b
afonsocrg <afonso.crg@gmail.com>
2020-03-18 18:31:58 +0000

    defined web service

--- backend/src/main/java/pt/ulisboa/tecnico/socialsoftware/tutor/question/api/StudentQuestionController.java
+++ backend/src/main/java/pt/ulisboa/tecnico/socialsoftware/tutor/question/api/StudentQuestionController.java
@@ -4,14 +4,21 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
-import pt.ulisboa.tecnico.socialsoftware.tutor.exceptions.ErrorMessage;
 import pt.ulisboa.tecnico.socialsoftware.tutor.exceptions.TutorException;
 import pt.ulisboa.tecnico.socialsoftware.tutor.question.StudentSubmitQuestionService;
+import pt.ulisboa.tecnico.socialsoftware.tutor.question.TeacherEvaluatesStudentQuestionService;
+import pt.ulisboa.tecnico.socialsoftware.tutor.question.domain.StudentQuestion;
 import pt.ulisboa.tecnico.socialsoftware.tutor.question.dto.StudentQuestionDTO;
-import pt.ulisboa.tecnico.socialsoftware.tutor.user.User;
+import pt.ulisboa.tecnico.socialsoftware.tutor.user.User;;
+
 import javax.validation.Valid;
 
 import java.security.Principal;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import pt.ulisboa.tecnico.socialsoftware.tutor.exceptions.ErrorMessage;
+import static pt.ulisboa.tecnico.socialsoftware.tutor.exceptions.ErrorMessage.INVALID_STUDENT_QUESTION_EVALUATION;
 
 
 @RestController
@@ -19,6 +26,9 @@ public class StudentQuestionController {
 
     @Autowired
     StudentSubmitQuestionService studentSubmitQuestionService;
+
+    @Autowired
+    TeacherEvaluatesStudentQuestionService teacherEvaluatesStudentQuestionService;
 
     /* ===========================================
      * F1: Student check suggested question status
@@ -35,5 +45,32 @@ public class StudentQuestionController {
         }
         return studentSubmitQuestionService.studentSubmitQuestion(courseId, studentQuestion, user.getId());
     }
-    
+
+
+
+    /* ===========================================
+     * F2: Teacher evaluates student question
+     * ===========================================
+     */
+
+    @GetMapping("/courses/{courseId}/studentQuestions")
+    @PreAuthorize("hasRole('ROLE_TEACHER') and hasPermission(#courseId, 'COURSE.ACCESS')")
+    public List<StudentQuestionDTO> getCourseStudentQuestions(@PathVariable int courseId){
+        return teacherEvaluatesStudentQuestionService.getAllStudentQuestions(courseId);
+    }
+
+    @PutMapping("/courses/{courseId}/studentQuestions/{studentQuestionId}/{evaluation}")
+    @PreAuthorize("hasRole('ROLE_TEACHER') and hasPermission(#courseId, 'COURSE.ACCESS')")
+    public StudentQuestionDTO evaluateStudentQuestion(@PathVariable int studentQuestionId, @PathVariable String evaluation, @Valid @RequestBody String justification){
+        StudentQuestion.SubmittedStatus newStatus = StudentQuestion.SubmittedStatus.valueOf(evaluation);
+        switch (newStatus) {
+            case APPROVED:
+                return teacherEvaluatesStudentQuestionService.acceptStudentQuestion(studentQuestionId, justification);
+            case REJECTED:
+                return teacherEvaluatesStudentQuestionService.rejectStudentQuestion(studentQuestionId, justification);
+            default:
+                throw new TutorException(INVALID_STUDENT_QUESTION_EVALUATION);
+        }
+    }
+
 }
