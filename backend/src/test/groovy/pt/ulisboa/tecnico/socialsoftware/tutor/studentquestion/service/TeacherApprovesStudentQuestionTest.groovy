30dc102819de36557931c974e4e73d08e3488d98
afonsocrg <afonso.crg@gmail.com>
2020-03-18 18:32:32 +0000

    [BUG FIX] improve tests: complete Option object

--- backend/src/test/groovy/pt/ulisboa/tecnico/socialsoftware/tutor/studentquestion/service/TeacherApprovesStudentQuestionTest.groovy
+++ backend/src/test/groovy/pt/ulisboa/tecnico/socialsoftware/tutor/studentquestion/service/TeacherApprovesStudentQuestionTest.groovy
@@ -77,7 +77,11 @@ class TeacherApprovesStudentQuestionTest extends Specification {
     private StudentQuestion createStudentQuestion(User user, Course course) {
         def studentQuestion = new StudentQuestion()
         studentQuestion.addTopic(new Topic())
-        studentQuestion.addOption(new Option())
+
+        Option o = new Option()
+        o.setCorrect(true)
+        o.setQuestion(studentQuestion)
+        studentQuestion.addOption(o)
         studentQuestion.setKey(STUDENT_QUESTION_KEY)
         studentQuestion.setStudentQuestionKey(STUDENT_QUESTION_KEY)
         studentQuestion.setUser(user)
