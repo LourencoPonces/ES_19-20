d7b0d134a9f17dc9698d625605d147da1cd30a78
André Breda <andrebreda@tecnico.ulisboa.pt>
2020-03-22 23:56:15 +0000

    backend/jmeter/runall.sh: Make Ctrl+C handling more predictable

--- backend/jmeter/runall.sh
+++ backend/jmeter/runall.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# exit whole script when Ctrl+C is pressed (instead of stopping current test)
+trap "exit" INT
+
 for test in **/*.jmx; do
 	echo "Running test: $test"
 	./runone.sh $test $@
