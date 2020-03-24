1ac0ffe1cc74eaf0f8eb0f2e7f91588706808792
afonsocrg <afonso.crg@gmail.com>
2020-03-24 20:27:58 +0000

    update script and tests to create and delete topic

--- backend/jmeter/studentQuestion/generate_StudentQuestionCreationSQL.sh
+++ backend/jmeter/studentQuestion/generate_StudentQuestionCreationSQL.sh
@@ -3,29 +3,27 @@
 BASE_ID=10001
 NUM_ROUNDS=5
 
-TOPIC_CONTENT="CHECK_STUDENT_QUESTION_STATUS_TOPIC"
+COURSE_ID=2
+
+TOPIC_ID=1000
+TOPIC_NAME="Testing topic"
+
+OPTION_CONTENT="CHECK_STUDENT_QUESTION_STATUS_OPTION"
+
 STATUS=("WAITING_FOR_APPROVAL" "APPROVED" "REJECTED")
+# STATUS=("WAITING_FOR_APPROVAL")
+
 JUSTIFICATION="Inappropriate question"
 JUST_IND=2 # only justify questions in this index. I know... super hard coded :(
-COURSE_ID=2
+
 
 echo ""
 
+echo "INSERT INTO topics(id, name, course_id) VALUES(${TOPIC_ID}, '${TOPIC_NAME}', ${COURSE_ID});"
 
-
-# for teacher in `seq 0 $((${NT} - 1))`; do
-
-#     for class in `seq 0 $((${NC} - 1))`; do
-        # ignore waiting for approval questions
-#         if [[ "${class}" -eq 4 ]]; then continue; fi
-
-        # ignore reject with no justification
-#         if [[ "${class}" -eq 3 ]]; then continue; fi
-# 
-#         for question in `seq 0 $((${QPC} - 1))`; do
+echo ""
 
 #Criar pergunta
-
 total_questions=0
 for round in `seq 0 $((${NUM_ROUNDS} - 1))`; do
     for status_ind in `seq 0 $((${#STATUS[@]} - 1))`; do
@@ -39,8 +37,8 @@ for round in `seq 0 $((${NUM_ROUNDS} - 1))`; do
         fi
         
         echo "INSERT INTO questions(id, title, content, course_id, status) VALUES(${qid}, 'Question${qid}', 'Content${qid}', ${COURSE_ID}, 'DISABLED');"
-        echo "INSERT INTO topics_questions(topics_id, questions_id) VALUES(125, ${qid});"
-        echo "INSERT INTO options(question_id, content, correct) VALUES(${qid}, '${TOPIC_CONTENT}', true);"
+        echo "INSERT INTO topics_questions(topics_id, questions_id) VALUES(${TOPIC_ID}, ${qid});"
+        echo "INSERT INTO options(question_id, content, correct) VALUES(${qid}, '${OPTION_CONTENT}', true);"
         echo "INSERT INTO student_question(id, user_id, submitted_status, justification, student_question_key) VALUES(${qid}, 676, '${question_status}', '${justification}', ${qid});"
         echo ""
 
@@ -55,10 +53,12 @@ echo ""
 
 # Cleanup
 # apagar primeiro a student question e depois a question
+echo "--Teardown"
 echo "DELETE FROM student_question;"
-echo "DELETE FROM options where content = '${TOPIC_CONTENT}';"
+echo "DELETE FROM options where content = '${OPTION_CONTENT}';"
 echo "DELETE FROM topics_questions where questions_id >= ${BASE_ID} and questions_id <= $((${BASE_ID} + ${total_questions}));"
 echo "DELETE FROM questions where id >= ${BASE_ID} and id < $((${BASE_ID} + ${total_questions}));"
+echo "DELETE FROM topics where id = ${TOPIC_ID};"
 
 
 printf "\n---------------------------------------------------\n"
