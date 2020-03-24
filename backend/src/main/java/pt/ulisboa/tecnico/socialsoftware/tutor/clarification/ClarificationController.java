ed4fce4da002eeaca6a644a3318d1795893e0654
André Breda <andrebreda@tecnico.ulisboa.pt>
2020-03-24 00:01:30 +0000

    ClarificationController: Expose clarification answer submission

--- backend/src/main/java/pt/ulisboa/tecnico/socialsoftware/tutor/clarification/ClarificationController.java
+++ backend/src/main/java/pt/ulisboa/tecnico/socialsoftware/tutor/clarification/ClarificationController.java
@@ -1,12 +1,26 @@
 package pt.ulisboa.tecnico.socialsoftware.tutor.clarification;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.*;
+import pt.ulisboa.tecnico.socialsoftware.tutor.clarification.dto.ClarificationRequestAnswerDto;
+import pt.ulisboa.tecnico.socialsoftware.tutor.user.User;
+
+import java.security.Principal;
 
 @RestController
 public class ClarificationController {
 
     @Autowired
     private ClarificationService clarificationService;
+
+    @PutMapping("/clarifications/{requestId}/answer")
+    @PreAuthorize("hasRole('ROLE_TEACHER') and hasPermission(#requestId, 'CLARIFICATION.SUBMIT_ANSWER')")
+    public ClarificationRequestAnswerDto submitAnswer(Principal principal, @PathVariable int requestId, @RequestBody String answer) {
+        User user = (User) ((Authentication) principal).getPrincipal();
+
+        return clarificationService.submitClarificationRequestAnswer(user, requestId, answer);
+    }
 
 }
