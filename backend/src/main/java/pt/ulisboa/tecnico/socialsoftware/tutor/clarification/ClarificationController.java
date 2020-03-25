784a2077211b844ad730e7ed4ca4b267bfd7d4c3
André Breda <andrebreda@tecnico.ulisboa.pt>
2020-03-25 19:43:00 +0000

    clarifications: Unify controllers
    I don't see more than one coming yet so there's no need for a different
    package.

--- backend/src/main/java/pt/ulisboa/tecnico/socialsoftware/tutor/clarification/ClarificationController.java
+++ backend/src/main/java/pt/ulisboa/tecnico/socialsoftware/tutor/clarification/ClarificationController.java
@@ -5,10 +5,12 @@ import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 import pt.ulisboa.tecnico.socialsoftware.tutor.clarification.dto.ClarificationRequestAnswerDto;
+import pt.ulisboa.tecnico.socialsoftware.tutor.clarification.dto.ClarificationRequestDto;
 import pt.ulisboa.tecnico.socialsoftware.tutor.exceptions.ErrorMessage;
 import pt.ulisboa.tecnico.socialsoftware.tutor.exceptions.TutorException;
 import pt.ulisboa.tecnico.socialsoftware.tutor.user.User;
 
+import javax.validation.Valid;
 import java.security.Principal;
 
 @RestController
@@ -16,6 +18,17 @@ public class ClarificationController {
 
     @Autowired
     private ClarificationService clarificationService;
+
+    @PostMapping("/student/results/questions/{questionId}/clarifications")
+    @PreAuthorize("hasRole('ROLE_STUDENT') and hasPermission(#questionId, 'QUESTION.ACCESS')")
+    public ClarificationRequestDto createClarificationRequest(@PathVariable int questionId, @Valid @RequestBody ClarificationRequestDto clarificationRequestDto, Principal principal) {
+        User user = (User) ((Authentication) principal).getPrincipal();
+
+        if (user == null) {
+            throw new TutorException(ErrorMessage.AUTHENTICATION_ERROR);
+        }
+        return clarificationService.submitClarificationRequest(questionId, user.getId(), clarificationRequestDto);
+    }
 
     @PutMapping("/clarifications/{requestId}/answer")
     @PreAuthorize("hasRole('ROLE_TEACHER') and hasPermission(#requestId, 'CLARIFICATION.ACCESS')")
