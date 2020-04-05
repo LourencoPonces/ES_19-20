dfe3f08c0be751c588530925573723ef647a1101
tpoliveira-mariana <mariana.tpdeoliveira@gmail.com>
2020-04-05 16:28:00 +0100

    Set up routing pages. Closes #144

---
+++ frontend/src/views/student/quiz/DiscussionComponent.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <h1>Discussion</h1>
+  </div>
+</template>
+
+<script lang="ts">
+import { Vue } from 'vue-property-decorator';
+
+export default class DiscussionComponent extends Vue {}
+</script>
+
+<style scoped></style>
