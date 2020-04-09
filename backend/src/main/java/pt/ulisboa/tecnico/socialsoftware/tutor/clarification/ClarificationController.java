dc800d43115c69ad5529d7a65c3f1a6d42d5c2fe
tpoliveira-mariana <mariana.tpdeoliveira@gmail.com>
2020-04-09 21:14:24 +0100

    Added controller to getStudentClarificationRequests.

--- backend/src/main/java/pt/ulisboa/tecnico/socialsoftware/tutor/clarification/ClarificationController.java
+++ backend/src/main/java/pt/ulisboa/tecnico/socialsoftware/tutor/clarification/ClarificationController.java
@@ -12,6 +12,7 @@ import pt.ulisboa.tecnico.socialsoftware.tutor.user.User;
 
 import javax.validation.Valid;
 import java.security.Principal;
+import java.util.List;
 
 @RestController
 public class ClarificationController {
@@ -28,6 +29,18 @@ public class ClarificationController {
             throw new TutorException(ErrorMessage.AUTHENTICATION_ERROR);
         }
         return clarificationService.submitClarificationRequest(questionId, user.getId(), clarificationRequestDto);
+    }
+
+    @GetMapping("/student/clarifications/")
+    @PreAuthorize("hasRole('ROLE_STUDENT')")
+    public List<ClarificationRequestDto> getStudentClarificationRequests(Principal principal) {
+        User user = (User) ((Authentication) principal).getPrincipal();
+
+        if (user == null) {
+            throw new TutorException(ErrorMessage.AUTHENTICATION_ERROR);
+        }
+
+        return clarificationService.getStudentClarificationRequests(user.getId());
     }
 
     @PutMapping("/clarifications/{requestId}/answer")
