0ca3b11a47a6f2c1f63582590d6ec9dedecd3830
Marcelo Santos <marcelocmsantos@gmail.com>
2020-04-15 13:50:00 +0100

    Implement Create Tournament dialog

--- frontend/src/views/student/tournament/TournamentsView.vue
+++ frontend/src/views/student/tournament/TournamentsView.vue
@@ -1,14 +1,55 @@
 <template>
   <!-- Remove -->
-  <h1>Tournaments</h1>
+  <v-card class="table">
+    <v-btn color="primary" dark @click="newTournament">New Tournament</v-btn>
+    <edit-tournament-dialog
+      v-if="currentTournament"
+      v-model="editTournamentDialog"
+      :tournament="currentTournament"
+      :topics="topics"
+    />
+  </v-card>
 </template>
 
 <script lang="ts">
-import { Component, Vue } from 'vue-property-decorator';
+import { Component, Vue, Watch } from 'vue-property-decorator';
 import RemoteServices from '@/services/RemoteServices';
+import Tournament from '@/models/management/Tournament';
+import Topic from '@/models/management/Topic';
+import EditTournamentDialog from '@/views/student/tournament/EditTournamentDialog.vue';
 
-@Component
-export default class TournamentsView extends Vue {}
+@Component({
+  components: {
+    'edit-tournament-dialog': EditTournamentDialog
+  }
+})
+export default class TournamentsView extends Vue {
+  currentTournament: Tournament | null = null;
+  editTournamentDialog: boolean = false;
+  topics!: Topic[];
+
+  newTournament() {
+    this.currentTournament = new Tournament();
+    this.editTournamentDialog = true;
+  }
+
+  @Watch('editTournamentDialog')
+  closeError() {
+    if (!this.editTournamentDialog) {
+      this.currentTournament = null;
+    }
+  }
+
+  async created() {
+    await this.$store.dispatch('loading');
+    try {
+      this.topics = await RemoteServices.getTopics();
+    } catch (error) {
+      await this.$store.dispatch('error', error);
+    }
+    await this.$store.dispatch('clearLoading');
+  }
+}
 </script>
 
 <style lang="scss" scoped></style>
