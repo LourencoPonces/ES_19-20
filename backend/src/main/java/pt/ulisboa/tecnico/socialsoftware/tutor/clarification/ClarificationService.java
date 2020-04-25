3eea97429c0cf4c4d9b889d5293fbbd0bc2dce63
tpoliveira-mariana <mariana.tpdeoliveira@gmail.com>
2020-04-25 17:12:28 +0100

    Implemented and tested backend. Closes #220 Closes #221

--- backend/src/main/java/pt/ulisboa/tecnico/socialsoftware/tutor/clarification/ClarificationService.java
+++ backend/src/main/java/pt/ulisboa/tecnico/socialsoftware/tutor/clarification/ClarificationService.java
@@ -46,6 +46,24 @@ public class ClarificationService {
     @PersistenceContext
     EntityManager entityManager;
 
+    @Retryable(
+            value = {SQLException.class},
+            backoff = @Backoff(delay = 5000))
+    @Transactional(isolation = Isolation.REPEATABLE_READ)
+    public ClarificationRequestDto changeClarificationRequestStatus(int reqId, boolean makePublic) {
+        ClarificationRequest req = clarificationRequestRepository.findById(reqId)
+                .orElseThrow(() -> new TutorException(ErrorMessage.CLARIFICATION_REQUEST_NOT_FOUND, reqId));
+
+        if (makePublic)
+            req.setStatus(ClarificationRequest.RequestStatus.PUBLIC);
+        else
+            req.setStatus(ClarificationRequest.RequestStatus.PRIVATE);
+
+        entityManager.persist(req);
+
+        return new ClarificationRequestDto(req);
+    }
+
 
     @Retryable(
             value = {SQLException.class},
@@ -189,6 +207,7 @@ public class ClarificationService {
     private ClarificationRequest createClarificationRequest(User user, Question question, ClarificationRequestDto clarificationRequestDto) {
         clarificationRequestDto.setOwner(user.getId());
         clarificationRequestDto.setQuestionId(question.getId());
+        clarificationRequestDto.setStatus(ClarificationRequest.RequestStatus.PRIVATE);
         ClarificationRequest clarificationRequest = new ClarificationRequest(user, question, clarificationRequestDto);
 
         if (clarificationRequestDto.getCreationDate() == null) {
@@ -258,4 +277,6 @@ public class ClarificationService {
                 .map(req -> req.getQuestion().getCourse().getId())
                 .orElse(-1);
     }
+
+
 }
