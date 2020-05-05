e957e14700ad8560fe02525dd7fa744062f973b0
Marcelo Santos <marcelocmsantos@gmail.com>
2020-05-05 20:11:54 +0100

    Add questions in tournament sign-up tests

--- backend/src/test/groovy/pt/ulisboa/tecnico/socialsoftware/tutor/tournament/service/SignUpInTournamentPerformanceTest.groovy
+++ backend/src/test/groovy/pt/ulisboa/tecnico/socialsoftware/tutor/tournament/service/SignUpInTournamentPerformanceTest.groovy
@@ -11,8 +11,10 @@ import pt.ulisboa.tecnico.socialsoftware.tutor.course.CourseExecutionRepository
 import pt.ulisboa.tecnico.socialsoftware.tutor.course.CourseRepository
 import pt.ulisboa.tecnico.socialsoftware.tutor.impexp.domain.AnswersXmlImport
 import pt.ulisboa.tecnico.socialsoftware.tutor.question.QuestionService
+import pt.ulisboa.tecnico.socialsoftware.tutor.question.domain.Question
 import pt.ulisboa.tecnico.socialsoftware.tutor.question.domain.Topic
 import pt.ulisboa.tecnico.socialsoftware.tutor.question.dto.TopicDto
+import pt.ulisboa.tecnico.socialsoftware.tutor.question.repository.QuestionRepository
 import pt.ulisboa.tecnico.socialsoftware.tutor.question.repository.TopicRepository
 import pt.ulisboa.tecnico.socialsoftware.tutor.quiz.QuizService
 import pt.ulisboa.tecnico.socialsoftware.tutor.tournament.repository.TournamentRepository
@@ -53,6 +55,9 @@ class SignUpInTournamentPerformanceTest extends Specification {
 
     @Autowired
     TournamentRepository tournamentRepository
+
+    @Autowired
+    QuestionRepository questionRepository
 
     def tournament
     def tournamentDto
@@ -104,6 +109,15 @@ class SignUpInTournamentPerformanceTest extends Specification {
         def topic = new Topic();
         topic.setName("TOPIC")
         topic.setCourse(course)
+
+        // So that quiz generation doesn't throw exceptions
+        def question = new Question()
+        question.addTopic(topic)
+        question.setTitle("question_title")
+        question.setCourse(course)
+        question.setStatus(Question.Status.AVAILABLE)
+
+        questionRepository.save(question)
         topicRepository.save(topic)
 
         def topicDto = new TopicDto(topic)
