258f871dc15a8dcb512b6d18d7af6a20110fd666
André Breda <andrebreda@tecnico.ulisboa.pt>
2020-06-06 00:48:59 +0100

    infra/backend-gce: Tag staging and stable builds

--- infra/backend-gce/build-publish-image.sh
+++ infra/backend-gce/build-publish-image.sh
@@ -15,9 +15,17 @@ popd
 
 
 # Build GCE-specialized backend image (requires the former)
-$DOCKER build -t "$REG_NAME:$VERSION" -t "$REG_NAME:latest" .
+$DOCKER build -t "$REG_NAME:$VERSION" -t "$REG_NAME:last" .
 
 # Publish GCE-specialized image to Container Registry
 gcloud auth configure-docker
-$DOCKER push "$REG_NAME:latest"
+$DOCKER push "$REG_NAME:last"
 $DOCKER push "$REG_NAME:$VERSION"
+if [ "a$(git branch --show-current)" = "amaster" ]; then
+	$DOCKER tag "$REG_NAME:$VERSION" "$REG_NAME:stable"
+	$DOCKER push "$REG_NAME:stable"
+fi
+if [ "a$(git branch --show-current)" = "adevelop" ]; then
+	$DOCKER tag "$REG_NAME:$VERSION" "$REG_NAME:staging"
+	$DOCKER push "$REG_NAME:staging"
+fi
