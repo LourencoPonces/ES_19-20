5edf591b8d6dc3fc7637424978030ca5a3a1772b
André Breda <andrebreda@tecnico.ulisboa.pt>
2020-06-08 16:04:07 +0100

    infra: Give backend access to the internet

--- infra/main.tf
+++ infra/main.tf
@@ -153,6 +153,25 @@ resource "google_compute_subnetwork" "backend_default" {
 	private_ip_google_access = true
 }
 
+resource "google_compute_router" "backend" {
+	name = "backend-router"
+	region = google_compute_subnetwork.backend_default.region
+	network = google_compute_network.backend.id
+}
+
+resource "google_compute_router_nat" "backend" {
+	name = "${google_compute_router.backend.name}-nat"
+	router = google_compute_router.backend.name
+	region = google_compute_router.backend.region
+	nat_ip_allocate_option = "AUTO_ONLY"
+	source_subnetwork_ip_ranges_to_nat = "ALL_SUBNETWORKS_ALL_IP_RANGES"
+
+	log_config {
+		enable = true
+		filter = "ERRORS_ONLY"
+	}
+}
+
 resource "google_compute_firewall" "backend_allow_icmp_ssh_http" {
 	name = "backend-allow-icmp-ssh-http"
 	network = google_compute_network.backend.id
