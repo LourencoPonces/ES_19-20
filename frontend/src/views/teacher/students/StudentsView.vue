8c8a1380e581345b9c3ec008076c715a7db7dad4
afonsocrg <afonso.crg@gmail.com>
2020-06-19 20:09:08 +0100

    student dropdown

--- frontend/src/views/teacher/students/StudentsView.vue
+++ frontend/src/views/teacher/students/StudentsView.vue
@@ -1,7 +1,9 @@
 /*<template>
   <div class="container">
     <h2>Students</h2>
-    <v-card class="table">
+
+    <!-- WEB BROWSER -->
+    <v-card class="table" v-if="!isMobile">
       <v-data-table
         :headers="headers"
         :items="students"
@@ -40,6 +42,98 @@
         </template>
       </v-data-table>
     </v-card>
+
+    <!-- MOBILE -->
+    <v-card class="table" v-else>
+      <v-data-table
+        :headers="headers_mobile"
+        :items="students"
+        :search="search"
+        disable-pagination
+        :hide-default-footer="true"
+        :mobile-breakpoint="0"
+      >
+        <template v-slot:top>
+          <v-card-title>
+            <v-text-field
+              v-model="search"
+              append-icon="search"
+              label="Search"
+              class="mx-2"
+            />
+
+            <v-spacer />
+          </v-card-title>
+        </template>
+        <template v-slot:item.name="{ item }">
+          <v-expansion-panels focusable>
+            <v-expansion-panel>
+              <v-expansion-panel-header>
+                <p>{{ item.name }}</p>
+              </v-expansion-panel-header>
+              <v-expansion-panel-content>
+                <v-row>
+                  <v-col cols="8">
+                    <span>Teacher Quizzes:</span>
+                  </v-col>
+                  <v-col>
+                    <span> {{ item.numberOfTeacherQuizzes }}</span>
+                  </v-col>
+                </v-row>
+                <v-row>
+                  <v-col cols="8">
+                    <span>Generated Quizzes:</span>
+                  </v-col>
+                  <v-col>
+                    <span> {{ item.numberOfStudentQuizzes }}</span>
+                  </v-col>
+                </v-row>
+                <v-row>
+                  <v-col cols="8">
+                    <span>Total Answers:</span>
+                  </v-col>
+                  <v-col>
+                    <span>{{ item.numberOfAnswers }}</span>
+                  </v-col>
+                </v-row>
+                <v-row>
+                  <v-col cols="8">
+                    <span>Correct Answers:</span>
+                  </v-col>
+                  <v-col>
+                    <v-chip
+                      :color="getPercentageColor(item.percentageOfCorrectAnswers)"
+                      dark
+                      >{{ item.percentageOfCorrectAnswers }}%</v-chip
+                    >
+                  </v-col>
+                </v-row>
+                <v-row>
+                  <v-col cols="8">
+                    <span>Answers Teacher Quiz</span>
+                  </v-col>
+                  <v-col>
+                    <span>{{ item.numberOfTeacherAnswers }}</span>
+                  </v-col>
+                </v-row>
+                <v-row>
+                  <v-col cols="8">
+                    <span>Correct Answers Teacher Quiz</span>
+                  </v-col>
+                  <v-col>
+                    <v-chip
+                      :color="getPercentageColor(item.percentageOfCorrectAnswers)"
+                      dark
+                      >{{ item.percentageOfCorrectTeacherAnswers }}%</v-chip
+                    >
+                  </v-col>
+                </v-row>
+              </v-expansion-panel-content>
+            </v-expansion-panel>
+          </v-expansion-panels>
+        </template>
+      </v-data-table>
+    </v-card>
   </div>
 </template>
 
@@ -51,6 +145,7 @@ import { Student } from '@/models/management/Student';
 
 @Component
 export default class StudentsView extends Vue {
+  isMobile: boolean = false;
   course: Course | null = null;
   students: Student[] = [];
   search: string = '';
@@ -94,8 +189,13 @@ export default class StudentsView extends Vue {
     }
   ];
 
+  headers_mobile: object = [
+    { text: 'Name', value: 'name', align: 'center', width: '40%' }
+  ];
+
   async created() {
     await this.$store.dispatch('loading');
+    this.isMobile = window.innerWidth <= 500;
     try {
       this.course = this.$store.getters.getCurrentCourse;
     } catch (error) {
